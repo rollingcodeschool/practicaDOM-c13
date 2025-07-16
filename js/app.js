c54a6be6e41cfc9fd7a748e095e0303ea4a2fd55
Emilse Arias <emilsearias@gmail.com>
2025-07-15 21:01:16 -0300

    agregamos la funcion crearTarea para adicionar cards usando createElement

--- js/app.js
+++ js/app.js
@@ -51,16 +51,49 @@ const obtenerNombre = (e)=>{
     parrafos[5].textContent += input[1].value
     formulario.reset()
 }
+
+const crearTarea = (e)=>{
+    e.preventDefault();
+    console.log('desde la funcion crear tarea')
+    //1- tomar la tarea del input
+    const inputTarea = document.getElementById('tarea').value.trim()
+    console.log(inputTarea)
+    //2- crear una card con el texto de la tarea anterior
+    const divcol = document.createElement('div'); //<div></div>
+    const divcard = document.createElement('div'); //<div></div>
+    const divcardBody = document.createElement('div'); //<div></div>
+    const h5 = document.createElement('h5'); //<div></div>
+    const p = document.createElement('p'); //<div></div>
+    divcol.className= 'col-md-4 col-lg-3'// <div class="col-md-4 col-lg-3">
+    divcard.className='card h-100'
+    divcardBody.className='card-body'
+    h5.className='card-title'
+    h5.textContent = 'Tarea ✔'
+    p.className = 'card-text'
+    p.textContent = inputTarea
+
+    const row = document.querySelector('.row-gap-3')
+    //3- agregar la card a la grilla
+    row.appendChild(divcol)
+    divcol.appendChild(divcard)
+    divcard.appendChild(divcardBody)
+    divcardBody.appendChild(h5)
+    // divcardBody.prepend(p)// este metodo agrega un nodo hijo al inicio
+    divcardBody.appendChild(p)// este metodo agrega un nodo hijo al final
+    //4-resetear el formulario
+    formularioTarea.reset()
+}
 //buscar el boton rojo
 // const btnRojo = document.getElementsByTagName('button')
 // const btnRojo = document.getElementsByClassName('btn-danger')
 const btnRojo = document.querySelector('.btn-danger')
 const btnAmarillo = document.querySelector('.btn-warning')
 const formulario = document.querySelector('#formNombre')
-console.log(formulario)
+const formularioTarea = document.querySelector('#formTareas')
+console.log(formularioTarea)
 
 //agregar el evento click
 btnRojo.addEventListener('click', ocultarParrafo)
 btnAmarillo.addEventListener('click', borrarParrafo)
 formulario.addEventListener('submit', obtenerNombre)
-
+formularioTarea.addEventListener('submit', crearTarea)
